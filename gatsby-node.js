c39db81acd001282220467d7120eeeeb5baedf42
Dácio Lima <daciolima@MacBook-Pro.local>
2020-11-18 15:00:16 -0300

    criaçao do campo Slug e pagina template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,51 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require('path')
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// You can delete this file if you're not using it
+// Adicionando um campo slug em cada post
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  // Verificando se o arquivo é markdown
+  if (node.internal.type === "MarkdownRemark") {
+    // Use `createFilePath` to turn markdown files in our `data/faqs` directory into `/faqs/slug`
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: "pages",
+    })
+
+    // Criando o campo slug dentro de cada nó
+    createNodeField({
+      node,
+      name: "slug",
+      value: `/${slug.slice(12)}`,
+    })
+  }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+      result.data.allMarkdownRemark.edges.forEach(({node}) => {
+          createPage({
+              path: node.fields.slug,
+              component: path.resolve('./src/templates/blog-post.js'),
+              context: {
+                  slug: node.fields.slug
+              }
+          })
+      })
+  })
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,53 +3,57 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
-import {useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 const IndexPage = () => {
-
   const { allMarkdownRemark } = useStaticQuery(graphql`
-  
-  query PostList {
-  allMarkdownRemark {
-    edges {
-      node {
-        frontmatter {
-          background
-          category
-          date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
-          description
-          title
+    query PostList {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              background
+              category
+              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+              description
+              title
+            }
+            id
+            timeToRead
+          }
         }
-        id
-        timeToRead
       }
     }
-  }
-}
-`)
+  `)
 
-const postList = allMarkdownRemark.edges
-
+  const postList = allMarkdownRemark.edges
 
   return (
     <Layout>
-    <SEO title="Home" />
-    {postList.map(({node: {frontmatter: {
-      background, category, date, description, title},
-      timeToRead 
-      },
-    }) => (
-        <PostItem 
-          slug='/about'
-          background={background}
-          category={category}
-          date={date}
-          timeToRead={timeToRead}
-          title={title}
-          description={description}
-        />
-  ))}
-  </Layout>
+      <SEO title="Home" />
+      {postList.map(
+        ({
+          node: {
+            frontmatter: { background, category, date, description, title },
+            timeToRead,
+            fields: {slug},
+          },
+        }) => (
+          <PostItem
+            slug={slug}
+            background={background}
+            category={category}
+            date={date}
+            timeToRead={timeToRead}
+            title={title}
+            description={description}
+          />
+        )
+      )}
+    </Layout>
   )
 }
 
