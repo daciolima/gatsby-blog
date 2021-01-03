0d6b7c7535025d578408a897047dee79a20e295a
Dácio Lima <daciolima@MacBook-Pro.local>
2021-01-03 01:06:59 -0300

    conf: git config core.ignorecase false

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
+    <SEO title="404: Página não encontrada" />
     <h1>404: Not Found</h1>
     <p>Ops...não tem nada aqui.</p>
   </Layout>
